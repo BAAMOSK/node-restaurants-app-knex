73d0c87e2628fa3f76f40a9881e500c92b9a2a7a
BAAMOSK <bunth667@gmail.com>
2017-04-25 12:00:27 -0400

    got the thai restaurants

--- drills.js
+++ drills.js
@@ -2,12 +2,21 @@
 // Require Knex and make connection
 const knex = require('knex')({
   client: 'pg',
-  connection: {
-    database: 'dev-restaurants-app'
-  },
+  connection: 'postgres://jfmpicvc:PRwM5rSx3YGUO71IkjtL9WO__0fA-u0M@stampy.db.elephantsql.com:5432/jfmpicvc'
 });
 
-// Sample select 
-knex.select('id', 'name', 'borough', 'cuisine')
-  .from('restaurants')
-  .then(results => console.log(results));
+
+
+// knex.select().table('restaurants').then(results => console.log(results));
+//knex.select().table('restaurants').where({cuisine: 'Italian'}).then(results => console.log(results));
+
+// knex.select('id', 'name')
+// .from('restaurants')
+// .where({cuisine: 'Italian'})
+// //.limit(10)
+// .then(names => console.log({restaurants: names}));
+
+knex('id', 'name')
+.count().where({cuisine: 'Thai'})
+.from('restaurants')
+.then(results => console.log(results)); 
