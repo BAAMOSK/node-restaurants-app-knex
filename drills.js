324ab34f7d3efaf4e406b59056936bb6b1abb3f6
BAAMOSK <bunth667@gmail.com>
2017-04-25 21:41:45 -0400

    treeize is deprecated

--- drills.js
+++ drills.js
@@ -3,13 +3,12 @@ let express = require('express');
 let app = express();
 let port = 3000;
 let Treeize = require('treeize');
+let sumsing;
 // Require Knex and make connection
 const knex = require('knex')({
   client: 'pg',
   connection: 'postgres://jfmpicvc:PRwM5rSx3YGUO71IkjtL9WO__0fA-u0M@stampy.db.elephantsql.com:5432/jfmpicvc'
 });
-
-
 
 app.get('/restaurants/:id', (request, response) => {
   knex.select('restaurants.id', 'name', 'cuisine', 'borough', 'grades.id as gradeId', 'grade', 'score')
@@ -17,9 +16,19 @@ app.get('/restaurants/:id', (request, response) => {
     .where({'restaurants.id': request.params.id})
     .innerJoin('grades', 'restaurants.id', 'grades.restaurant_id')
     .then(results => {
-      var items = new Treeize();
-      items.grow(results);
-      response.items.getData();
+      let cahka = results;
+      let items = new Treeize();
+      items.grow(cahka);
+      sumsing = items.getData();
+      console.log(sumsing);
+      return response.json(sumsing);
+      // let grades = new Treeize();
+      // grades.grow(results);
+      // sumsing = grades.getData();
+      // response.json(sumsing);
+      // var items = new Treeize();
+      // items.grow(results);
+      // response.items.getData();
       //response.json(results);
       //var tree = items.getData();
       //console.log(response.json(tree));
